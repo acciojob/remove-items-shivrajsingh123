31e626a3f0ddf9b446e0a8d92e4398e641c43993
shivrajsingh123 <45819719+shivrajsingh123@users.noreply.github.com>
2025-03-16 17:12:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ document.getElementById("removeButton").addEventListener("click", () => {
+        const colorSelect = document.getElementById("colorSelect");
+        if (colorSelect.selectedIndex !== -1) {
+            colorSelect.remove(colorSelect.selectedIndex);
+        }
+    });
+});
